7da670ae7dd91a05140b17cf2a0a66282305a487
kyriosdata <fabio@inf.ufg.br>
2021-12-23 19:50:55 -0300

    tentando rodar cálculo de forma assíncrona

--- web/src/App.js
+++ web/src/App.js
@@ -4,13 +4,32 @@ import "./App.css";
 import d44 from "desafio4x4";
 import Matriz from "./Matriz";
 
-function App() {
-    let todasAsSolucoes;
+function GerandoSolucoes() {
+    return (
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1>Gerando soluções. Aguarde...</h1>
+        </div>
+    );
+}
 
+function App() {
     const [solucoes, setSolucoes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    async function calcula() {
+        return d44();
+    }
+
+    useEffect(() => {
+        (async () => {
+            const resposta = await calcula();
+            setSolucoes(resposta.solucoes);
+            setLoading(false);
+        })();
+    }, []);
 
     function exibe(matrizes) {
-        console.log(matrizes);
         if (!matrizes) {
             return;
         }
@@ -18,16 +37,12 @@ function App() {
         return matrizes.map((m, i) => <Matriz key={i} valor={m}/>);
     }
 
-    useEffect(() => {
-        const resposta = d44();
-        todasAsSolucoes = resposta.solucoes;
-        setSolucoes(todasAsSolucoes);
-        console.log("Executado em (ms):", resposta.tempo);
-    }, []);
+    if (loading) {
+        return GerandoSolucoes();
+    }
 
     return (
         <div className="container">
-            <img src={logo} className="App-logo" alt="logo"/>
             <h1>Total de soluções: {solucoes.length}</h1>
             {exibe(solucoes)}
         </div>
