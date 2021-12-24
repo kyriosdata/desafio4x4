070cfc8ec0b6a08646c99f09d0af621dcf23b908
kyriosdata <fabio@inf.ufg.br>
2021-12-24 17:26:29 -0300

    arquivo de soluções recuperado de ponto remoto

--- web/src/App.js
+++ web/src/App.js
@@ -1,52 +1,49 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import logo from "./d44.png";
 import "./App.css";
-import d44 from "desafio4x4";
 import Matriz from "./Matriz";
+import axios from "axios";
 
-function GerandoSolucoes() {
-    return (
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1>Gerando soluções. Aguarde...</h1>
-        </div>
-    );
+function GerandoSolucoes(x) {
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Baixando soluções. Aguarde...</h1>
+    </div>
+  );
 }
 
 function App() {
-    const [solucoes, setSolucoes] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [solucoes, setSolucoes] = useState();
 
-    async function calcula() {
-        return d44();
-    }
-
-    useEffect(() => {
-        (async () => {
-            const resposta = await calcula();
-            setSolucoes(resposta.solucoes);
-            setLoading(false);
-        })();
-    }, []);
-
-    function exibe(matrizes) {
-        if (!matrizes) {
-            return;
-        }
-
-        return matrizes.map((m, i) => <Matriz key={i} valor={m}/>);
-    }
-
-    if (loading) {
-        return GerandoSolucoes();
-    }
-
-    return (
-        <div className="container">
-            <h1>Total de soluções: {solucoes.length}</h1>
-            {exibe(solucoes)}
-        </div>
+  useEffect(async () => {
+    const recuperado = await axios(
+      "https://unpkg.com/desafio4x4@1.0.3/solucoes.json"
     );
+
+    setSolucoes(recuperado.data);
+    setLoading(false);
+  }, []);
+
+  function exibe(matrizes) {
+    if (!matrizes) {
+      return;
+    }
+
+    return matrizes.map((m, i) => <Matriz key={i} valor={m} />);
+  }
+
+  if (loading) {
+    return GerandoSolucoes("teste");
+  }
+
+  return (
+    <div className="container">
+      <h1>Total de soluções: {solucoes.length}</h1>
+      {exibe(solucoes)}
+    </div>
+  );
 }
 
 export default App;
